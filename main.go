51e15848220fe7eff3a2e28c99f37519cc518ce0
Vaclav Dvorak <vaclav.dvorak@bootiq.io>
2023-08-22 10:04:56 +0200

    feat: Sort table by 1y score

--- main.go
+++ main.go
@@ -26,7 +26,8 @@ func main() {
 		}
 		years[year] = dcaData
 	}
-	dcaWeekS := years[yearNum-1].week
+	var dcaWeekS []stat
+	dcaWeekS = append(dcaWeekS, years[0].week...) // we need deep copy of slice
 	sort.Slice(dcaWeekS, func(i, j int) bool {
 		return dcaWeekS[i].score > dcaWeekS[j].score
 	})
@@ -36,7 +37,7 @@ func main() {
 	t.AppendHeader(table.Row{"day", "price", "count", "score (5y)", "score (3y)", "score (1y)"})
 	for _, v := range dcaWeekS {
 		t.AppendRow(table.Row{
-			dow[v.date], fmt.Sprintf("%0.0f %s", v.avg, cur), v.count, fmt.Sprintf("%0.3f", v.score*100), fmt.Sprintf("%0.3f", years[2].week[v.date].score*100), fmt.Sprintf("%0.3f", years[0].week[v.date].score*100),
+			dow[v.date], fmt.Sprintf("%0.0f %s", v.avg, cur), v.count, fmt.Sprintf("%0.3f", years[4].week[v.date].score*100), fmt.Sprintf("%0.3f", years[2].week[v.date].score*100), fmt.Sprintf("%0.3f", years[0].week[v.date].score*100),
 		})
 	}
 	t.SetStyle(table.StyleLight)
